b5eb1485a181a0e9afaf50f596ba2da506b021b6
Thane Somers <thanesomers@gmail.com>
2017-02-27 18:02:34 -0800

    Add Absolute Ratings
    This isn't working now and the code's a mess from debugging, but it's a
    start?

--- GP_preference_demo.py
+++ GP_preference_demo.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 import GPpref
 import scipy.optimize as op
 
-log_hyp = np.log([0.1,1.0,0.1]) # length_scale, sigma_f, sigma_probit
-np.random.seed(1)
+log_hyp = np.log([0.1,1.0,0.1,10.0]) # length_scale, sigma_f, sigma_probit, v_beta
+np.random.seed(2)
 
-n_train = 20
+n_rel_train = 0
+n_abs_train = 5
 true_sigma = 0.05
 delta_f = 1e-5
 
 # Define polynomial function to be modelled
 def true_function(x):
-    y = np.sin(x*2*np.pi + np.pi/4) + 0.2
+    y = (np.sin(x*2*np.pi + np.pi/4) + 1)/2
+    #y = np.sin(x*2.0*np.pi + np.pi/4.0)
     return y
 
 # Define noisy observation function
@@ -32,39 +34,63 @@ def noisy_preference_rank(uv, sigma):
 # Plot true function
 x_plot = np.linspace(0.0,1.0,101,dtype='float')
 y_plot = true_function(x_plot)
+hf,ha = plt.subplots(1,1)
+ha.plot(x_plot,y_plot,'r-')
+
 
 # Training data - this is a bit weird, but we sample x values, then the uv pairs
 # are actually indexes into x, because it is easier computationally. You can 
 # recover the actual u,v values using x[ui],x[vi]
-x_train = np.random.random((2*n_train,1))
-uvi_train = np.random.choice(range(2*n_train), (n_train,2), replace=False)
-uv_train = x_train[uvi_train][:,:,0]
+if n_rel_train > 0:
+    x_train = np.random.random((2*n_rel_train,1))
+    uvi_train = np.random.choice(range(2*n_rel_train), (n_rel_train,2), replace=False)
+    uv_train = x_train[uvi_train][:,:,0]
 
-# Get noisy observations f(uv) and corresponding ranks y_train
-y_train, fuv_train = noisy_preference_rank(uv_train,true_sigma)
+    # Get noisy observations f(uv) and corresponding ranks y_train
+    y_train, fuv_train = noisy_preference_rank(uv_train, true_sigma)
+    for uv,fuv,y in zip(uv_train, fuv_train, y_train):
+        ha.plot(uv, fuv, 'b-')
+        ha.plot(uv[(y+1)/2],fuv[(y+1)/2],'k+')
 
-hf,ha = plt.subplots(1,1)
-ha.plot(x_plot,y_plot,'r-')
-for uv,fuv,y in zip(uv_train, fuv_train, y_train):
-    ha.plot(uv, fuv, 'b-')
-    ha.plot(uv[(y+1)/2],fuv[(y+1)/2],'k+')
+else:
+    x_train = np.zeros((0,1))
+    uvi_train = np.zeros((0,2))
+    uv_train = np.zeros((0,2))
+    y_train = np.zeros((0,1))
+    fuv_train = np.zeros((0,2))
+
+x_abs_train = np.random.random((n_abs_train,1))
+#y_abs_train = obs_function(x_abs_train, true_sigma)
+y_abs_train = np.clip(obs_function(x_abs_train, true_sigma), 0.01, .99)
+
+ha.plot(x_abs_train, y_abs_train, 'r+')
 
 ha.set_title('Training data')
 ha.set_ylabel('f(x)')
 ha.set_xlabel('x')
 
-
 # GP hyperparameters
 # note: using log scaling to aid learning hyperparameters with varied magnitudes
 
-prefGP = GPpref.PreferenceGaussianProcess(x_train, uvi_train, y_train, delta_f=delta_f)
+print "Data"
+# print x_train
+print x_abs_train
+# print y_train
+print y_abs_train
+# print uvi_train
+
+prefGP = GPpref.PreferenceGaussianProcess(x_train, uvi_train, x_abs_train,  y_train, y_abs_train, delta_f=delta_f)
 
 # Pseudocode:
 # FOr a set of hyperparameters, return log likelihood that can be used by an optimiser
 theta0 = log_hyp
 
 # log_hyp = op.fmin(prefGP.calc_nlml,theta0)
-f,lml = prefGP.calc_laplace(log_hyp)
+#f,lml = prefGP.calc_laplace(log_hyp)
+f = prefGP.calc_laplace(log_hyp)
 
-ha.plot(x_train, f, 'g^')
+if x_train.shape[0]>0:
+    ha.plot(x_train, f[0:x_train.shape[0]], 'g^')
+if x_abs_train.shape[0]>0:
+    ha.plot(x_abs_train, f[x_train.shape[0]:], 'r^')
 plt.show()
--- GPpref.py
+++ GPpref.py
@@ -1,13 +1,21 @@
 # Simple 1D GP classification example
 import numpy as np
 import GPy
-from scipy.special import ndtr as std_norm_cdf
+#from scipy.special import ndtr as std_norm_cdf, digamma, polygamma
+from scipy.special import digamma, polygamma
+from scipy.stats import norm
+from scipy.linalg import block_diag
 
 #define a standard normal pdf
 _sqrt_2pi = np.sqrt(2*np.pi)
 def std_norm_pdf(x):
     x = np.clip(x,-1e150,1e150)
-    return np.exp(-(x**2)/2)/_sqrt_2pi
+    return norm.pdf(x)
+    #return np.exp(-(x**2)/2)/_sqrt_2pi
+
+def std_norm_cdf(x):
+    x = np.clip(x, -30, 100 )
+    return norm.cdf(x)
 
 # Define squared distance calculation function
 def squared_distance(A,B):
@@ -85,6 +93,7 @@ class PrefProbit(object):
             W[yi, yi] -= ddpy_df  # If x_i = x_i = v_k then I(x_i)*I(y_i) = -1*-1 = 1
             W[xi, yi] -= -ddpy_df  # Otherwise, I(x_i)*I(y_i) = -1*1 = -1
             W[yi, xi] -= -ddpy_df
+
         return W, dpy_df
 
     def log_marginal(self, uvi, y, f, iK, logdetK):
@@ -93,18 +102,116 @@ class PrefProbit(object):
         psi = np.sum(np.log(phi_z)) - 0.5 * np.matmul(np.matmul(f.T, iK), f) - 0.5*logdetK - iK.shape[0]/2.0*self.log2pi
         return psi.flat[0]
 
+class AbsBoundProbit(object):
+    def __init__(self, sigma=1.0, v=10.0):
+        # Making v=10 looks good on a graph, but I'm not sure what it's actually supposed to be.  
+        self.set_sigma(sigma)
+        self.set_v(v)
+        self.log2pi = np.log(2.0*np.pi)
+
+    def set_sigma(self, sigma):
+        self.sigma = sigma
+        self._isqrt2sig = 1.0 / (self.sigma * np.sqrt(2.0))
+        self._i2var =  self._isqrt2sig**2
+
+    def set_v(self, v):
+        self.v = v
+
+    def mean_link(self, f):
+        return std_norm_cdf(f*self._isqrt2sig)
+
+    def alpha(self, f):
+        return self.v * self.mean_link(f)
+
+    def beta(self, f):
+        return self.v * (1-self.mean_link(f))
+
+    def derivatives(self, y, f):
+
+        print "Start Iter.  f, y"
+        print f
+        print y
+        alpha = self.alpha(f)
+        beta = self.beta(f) #let's make a distribution called beta that also has beta as a parameter!
+        # print "Alpha: " + str(alpha)
+        # print "Beta: " + str(beta) 
+
+        # Theres a dot in Jensen that I'm hoping is just a typo. I didn't fully derive it, but it looks correct.   
+        # As the iteration goes, f blows up.  This makes parts of alpha, beta go to v and 0.  Digamma(0)=-inf :(
+        # So why is it blowing up?
+        dpy_df = self.v*std_norm_pdf(f*self._isqrt2sig) * (np.log(y)-np.log(1-y) - digamma(alpha) + digamma(beta) )
+
+        Wdiag = ( - self.v**2*std_norm_pdf(f*self._isqrt2sig) * 
+                    ( std_norm_pdf(f*self._isqrt2sig) * ( polygamma(0, alpha) + polygamma(0, alpha) ) + 
+                    f*self._i2var/self.v * (np.log(y)-np.log(1-y)-digamma(alpha) + digamma(beta)) ))
+
+        W = np.diagflat(Wdiag)
+
+        return W, dpy_df
+
+    def log_marginal(self):
+        pass
+
+
+class AbsProbit(object):
+    # The Probit Likelihood given in Rasmussen, p43.  Note f(x) is scaled by sqrt(2)*sigma, as in Jensen.  
+    # Ok, yeah, this was totally wrong.  Don't use this.  
+    def __init__(self, sigma=1.0, v=10.0):
+        self.set_sigma(sigma)
+        self.log2pi = np.log(2.0*np.pi)
+
+    def set_sigma(self, sigma):
+        self.sigma = sigma
+        self._isqrt2sig = 1.0 / (self.sigma * np.sqrt(2.0))
+        self._i2var =  self._isqrt2sig**2
+
+    def derivatives(self, y, f):
+
+        print "Start Iter.  f, y"
+        print f
+        print y
+
+        # dpy_df = -y*std_norm_pdf(f*self._isqrt2sig) / std_norm_cdf(y*f*self._isqrt2sig)
+
+        # Wdiag = -(-np.power(std_norm_pdf(f*self._isqrt2sig) / std_norm_cdf(y*f*self._isqrt2sig),2)
+        #             -y*f*self._isqrt2sig*std_norm_pdf(f*self._isqrt2sig) / std_norm_cdf(y*f*self._isqrt2sig))
+
+        dpy_df = -y*std_norm_pdf(f) / std_norm_cdf(y*f)
+
+        Wdiag = -(-np.power(std_norm_pdf(f) / std_norm_cdf(y*f),2)
+                    -y*f*std_norm_pdf(f) / std_norm_cdf(y*f))
+
+        W = np.diagflat(Wdiag)
+
+        return W, dpy_df
+        
+        
+
 class PreferenceGaussianProcess(object):
 
-    def __init__(self, x_train, uvi_train, y_train, likelihood=PrefProbit, delta_f = 1e-6):
-        # log_hyp are log of hyperparameters, note that it is [length_0, ..., length_d, sigma_f, sigma_probit]
-        self._xdim = x_train.shape[1]
-        self._nx = x_train.shape[0]
+    def __init__(self, x_train, uvi_train, x_abs_train, y_train, y_abs_train, likelihood=PrefProbit, delta_f = 1e-6):
+        # log_hyp are log of hyperparameters, note that it is [length_0, ..., length_d, sigma_f, sigma_probit, v_beta]
+        # Training points are split into relative and absolute for calculating f, but combined for predictions.  
+        if x_train.shape[0] is not 0:
+            self._xdim = x_train.shape[1]
+        elif x_abs_train.shape[0] is not 0:
+            self._xdim = x_abs_train.shape[1]
+        else: 
+            raise Exception("No Input Points")
+        self._nx = x_train.shape[0] + x_abs_train.shape[0]
+        self._n_rel = x_train.shape[0]
+        self._n_abs = x_abs_train.shape[0]
         self.x_train = x_train
         self.y_train = y_train
+        self.x_abs_train = x_abs_train
+        self.y_abs_train = y_abs_train
         self.uvi_train = uvi_train
         self.delta_f = delta_f
 
-        self.likelihood = likelihood()
+        self.x_train_all = np.concatenate((self.x_train, self.x_abs_train), 0)
+
+        self.rel_likelihood = likelihood()
+        self.abs_likelihood = AbsBoundProbit()
 
         self.kern = GPy.kern.RBF(self._xdim, ARD=True)
 
@@ -112,14 +219,16 @@ class PreferenceGaussianProcess(object):
     def calc_laplace(self, loghyp, f=None):
         self.kern.lengthscale = np.exp(loghyp[0:self._xdim])
         self.kern.variance = (np.exp(loghyp[self._xdim]))**2
-        self.likelihood.set_sigma = np.exp(loghyp[-1])
+        self.rel_likelihood.set_sigma = np.exp(loghyp[-2]) # Do we need different sigmas for each likelihood?  Hopefully No?
+        self.abs_likelihood.set_sigma = np.exp(loghyp[-2])
+        #self.abs_likelihood.set_v = np.exp(loghyp[-1])
 
         if f is None:
             f = np.zeros((self._nx, 1))
 
         # With current hyperparameters:
         Ix = np.eye(self._nx)
-        K = self.kern.K(self.x_train)
+        K = self.kern.K(self.x_train_all)
         eps = 1e-6
         inv_ok = False
 
@@ -138,10 +247,36 @@ class PreferenceGaussianProcess(object):
         f_error = self.delta_f + 1
 
         while f_error > self.delta_f:
-            W, dpy_df = self.likelihood.derivatives(self.uvi_train, self.y_train, f)
+            # Is splitting these apart correct?  Will there be off-diagonal elements of W_abs that should be 
+            # non-zero because of the relative training points?
+            f_rel = f[0:self._n_rel]
+            f_abs = f[self._n_rel:]
+
+            # Get relative Hessian and Gradient
+            if self._n_rel>0 and self._n_abs>0:
+                W_rel, dpy_df_rel = self.rel_likelihood.derivatives(self.uvi_train, self.y_train, f_rel)
+                # Get Absolute Hessian and Gradient
+                # Note that y_abs_train has to be [0,1], which could be an issue.  
+                W_abs, dpy_df_abs = self.abs_likelihood.derivatives(self.y_abs_train, f_abs)
+
+                # Combine W, gradient
+                dpy_df = np.concatenate((dpy_df_rel, dpy_df_abs), axis=0)
+                W = block_diag(W_rel, W_abs)
+
+            elif self._n_rel>0:
+                W, dpy_df = self.rel_likelihood.derivatives(self.uvi_train, self.y_train, f_rel)
+
+            elif self._n_abs>0:
+                W, dpy_df = self.abs_likelihood.derivatives(self.y_abs_train, f_abs)
+
+            # print "Total"
+            print "Dpy, W:"
+            print dpy_df
+            print W
+
             g = (iK + W)
             f_new = np.matmul(np.linalg.inv(g), np.matmul(W, f) + dpy_df)
-            lml = self.likelihood.log_marginal(self.uvi_train, self.y_train, f_new, iK, logdetK)
+            #lml = self.rel_likelihood.log_marginal(self.uvi_train, self.y_train, f_new, iK, logdetK)
 
             ## Jensen version (iK + W)^-1 = K - K((I + WK)^-1)WK (not sure how to get f'K^-1f though...
             # ig = K - np.matmul(np.matmul(np.matmul(K, np.linalg.inv(Ix + np.matmul(W, K))), W), K)
@@ -151,10 +286,11 @@ class PreferenceGaussianProcess(object):
             df = np.abs((f_new - f))
             f_error = np.max(df)
 
-            print f_error,lml
+            print "F Error: " + str(f_error) #,lml
+            # print "F New: " + str(f_new)
             f = f_new
 
-        return f, lml
+        return f#, lml
 
     def calc_nlml(self, loghyp):
         f,lml = self.calc_laplace(loghyp)
