Trying to fix regression covariance because broken

GP_preference_demo.py
@@ -14,7 +14,7 @@ train_hyper = False
 use_test_data = False # test_data.data3 #
 verbose = 2
 
-with open('./data/statruns_2D.yaml', 'rt') as fh:
+with open('./data/low_freq_2.yaml', 'rt') as fh:
     wave = yaml.safe_load(fh)
 try:
     np.random.seed(wave['statrun_params']['randseed'])
@@ -24,10 +24,10 @@ d_x = wave['GP_params']['hyper_counts'][0]-1
 random_wave = test_data.MultiWave(n_dimensions=d_x, **wave['wave_params'])
 log_hyp = np.log(wave['hyperparameters'])
 
-n_rel_train = 15
+n_rel_train = 0
 n_abs_train = 2
 
-n_xplot = 21
+n_xplot = 101
 n_posterior_samples = 3
 
 random_wave.print_values()

GPpref.py
@@ -76,7 +76,7 @@ class SquaredExponential(object):
 class PrefProbit(object):
     type = 'preference'
     y_type = 'discrete'
-    y_list = np.array([-1, 1], dtype='int')
+    y_list = np.array([[-1], [1]], dtype='int')
 
     def __init__(self, sigma=1.0):
         self.set_hyper([sigma])
@@ -306,7 +306,7 @@ class OrdinalProbit(object):
 class AbsBoundProbit(object):
     type = 'bounded continuous'
     y_type = 'bounded'
-    y_list = np.linspace(0.01, 0.99, 101)
+    y_list = np.atleast_2d(np.linspace(0.01, 0.99, 101)).T
 
     def __init__(self, sigma=1.0, v=10.0):
         # v is the precision, kind of related to inverse of noise, high v is sharp distributions

GPr.py
@@ -47,10 +47,10 @@ class GaussianProcess(object):
         Kxx = self.covFun.compute_Kxx_matrix()
         iKxx = np.linalg.inv(Kxx)
         Kzx = Kxz.T
-        K_diag = np.diagonal(np.dot(np.dot(Kzx,iKxx),Kxz))
-        K_noise = self.covFun.sf2*np.ones(np.size(testInput,axis=0))
+        K_diag = np.dot(np.dot(Kzx,iKxx),Kxz)
+        K_noise = self.covFun.sn2*np.eye(np.size(testInput,axis=0))
         fz = np.dot(np.dot(Kzx,iKxx),self.trainTarget.T)
-        cov_fz = K_noise - K_diag
+        cov_fz = K_noise + K_diag
         return fz, cov_fz
 	
     # Define GP negative log marginal likelihood function
@@ -93,7 +93,7 @@ class SquaredExponential(CovarianceFunction):
         self.hyp = np.exp(self.logHyp)	# hyperparameters
         n = len(self.hyp)
         self.M = self.hyp[:n-2]        	# length scales
-        self.sf2 = self.hyp[n-2]**2        # squared exponential variance
+        self.sf2 = self.hyp[n-2]**2        # sigma_f variance
         self.sn2 = self.hyp[n-1]**2        # noise variance
 	
     def compute_Kxx_matrix(self):

plot_tools.py
@@ -28,7 +28,7 @@ def make_poly_array(x,y,sigma):
     return xy
 
 
-def plot_with_bounds(ax, x, y, s, c=lines[0]):
+def plot_with_bounds(ax, x, y, s, c=lines[0], lw=1.5, *args, **kwargs):
     isort = np.argsort(x.flat)
     xx, yy = x[isort], y[isort]
     try:
@@ -37,7 +37,7 @@ def plot_with_bounds(ax, x, y, s, c=lines[0]):
         ss = s
     xy = make_poly_array(xx, yy, ss)
     h_patch = Polygon(xy, ec=c, fc=lighten(c, 3), alpha=0.5)
-    h_fx, = ax.plot(xx, yy, lw=1.5, c=c)
+    h_fx, = ax.plot(xx, yy, lw=lw, c=c, *args, **kwargs)
     ax.add_patch(h_patch)
     clim = ax.get_ylim()
     ax.set_ylim(bottom = min(clim[0],xy[:,1].min()), top = max(clim[1], xy[:,1].max()))