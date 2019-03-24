7f3d49eac56f1c11ad0559a0a396ef3a116aa508
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2019-03-24 16:12:13 +0100

    Add wine data

---
+++ read_wine_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+class WineQualityData(object):
+
+    def __init__(self, data_file):
+        self.file = data_file
+        self.data = pandas.read_csv(self.file, delimiter=';')
+
+red_data = WineQualityData('data/wine_quality/winequality-red.csv')
+white_data = WineQualityData('data/wine_quality/winequality-white.csv')
+
+fh, ah = plt.subplots(1, 2)
+ah[0].hist(red_data.data.quality, np.arange(-0.5, 11, 1.0))
+ah[1].hist(white_data.data.quality, np.arange(-0.5, 11, 1.0))
+for a in ah:
+    a.set_xticks(np.arange(11))
+    a.set_xlabel('Score')
+ah[0].set_ylabel('Count')
+ah[0].set_title('Red wine')
+ah[1].set_title('White wine')
+
+plt.show(block=False)
